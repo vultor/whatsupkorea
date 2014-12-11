ca543eae7a79811b6d36d97a07369354115430de
Vultor <shawn@shawnroe.com>
2014-12-11 20:31:15 +0900

    Swipeleft on survey page to return to home page

--- js/wuk.js
+++ js/wuk.js
@@ -4,22 +4,4 @@ $(document).on('pagecreate', function(evt) {
 	$('#survey').on("swipeleft", function(e) {
 		$.mobile.changePage('#home');
 	}); //end survey swipeleft
-	$('rraddresses').on("swipeleft", function(e) {
-		$.mobile.changePage('#rrinternet');
-	});
-	$('rrinternet').on("swipeleft", function(e) {
-		$.mobile.changePage('#rrkoreanfood');
-	});
-	$('rrkoreanfood').on("swipeleft", function(e) {
-		$.mobile.changePage('#rr');
-	});
-	$('rraddresses').on("swiperight", function(e) {
-		$.mobile.changePage('#rr');
-	});
-	$('rrinternet').on("swiperight", function(e) {
-		$.mobile.changePage('#rraddresses');
-	});
-	$('rrkoreanfood').on("swiperight", function(e) {
-		$.mobile.changePage('#rrinternet');
-	});
 }); // end pagecreate
