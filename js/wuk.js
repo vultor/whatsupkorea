b2a10bb56271906f5f961eb6a04bdae8755c8a0e
Vultor <shawn@shawnroe.com>
2014-12-11 16:14:07 +0900

    Swipeleft and swiperight added to rant and rave articles

--- js/wuk.js
+++ js/wuk.js
@@ -3,5 +3,23 @@
 $(document).on('pagecreate', function(evt) {
 	$('#survey').on("swipeleft", function(e) {
 		$.mobile.changePage('#home');
-	}); //end swipeleft
+	}); //end survey swipeleft
+	$('rraddresses').on("swipeleft", function(e) {
+		$.mobile.changePage('#rrinternet');
+	});
+	$('rrinternet').on("swipeleft", function(e) {
+		$.mobile.changePage('#rrkoreanfood');
+	});
+	$('rrkoreanfood').on("swipeleft", function(e) {
+		$.mobile.changePage('#rr');
+	});
+	$('rraddresses').on("swiperight", function(e) {
+		$.mobile.changePage('#rr');
+	});
+	$('rrinternet').on("swiperight", function(e) {
+		$.mobile.changePage('#rraddresses');
+	});
+	$('rrkoreanfood').on("swiperight", function(e) {
+		$.mobile.changePage('#rrinternet');
+	});
 }); // end pagecreate
